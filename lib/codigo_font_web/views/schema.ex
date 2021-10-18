f244ed3046ed21e3aa4a2a88056998ebfee75a10
root <root@LAPTOP-6M8C8QD8.localdomain>
2021-10-18 14:21:17 -0300

    authorization

--- lib/codigo_font_web/views/schema.ex
+++ lib/codigo_font_web/views/schema.ex
@@ -2,12 +2,14 @@ defmodule CodigoFontWeb.Schema do
   use Absinthe.Schema
 
   alias CodigoFontWeb.Resolvers
+  alias CodigoFontWeb.Schemas.Middlewares
   import_types(CodigoFontWeb.Schema.Types)
 
   query do
     @desc "Get a list of all users"
     field :users, list_of(:user_type) do
       # Resolver
+      middleware(Middlewares.Authorization, :any)
       resolve(&Resolvers.UserResolver.users/3)
     end
   end
